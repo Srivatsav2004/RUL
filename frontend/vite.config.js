f741226e16769428649e03144f56d2c902c25ccf
Srivatsav2004 <153432234+Srivatsav2004@users.noreply.github.com>
2025-10-12 21:52:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://rul-backend.onrender.com",
+        target: "https://rul-3tso.onrender.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "")
